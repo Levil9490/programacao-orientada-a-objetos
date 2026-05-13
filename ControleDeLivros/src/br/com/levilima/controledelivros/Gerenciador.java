de2ebfc22282f289db51776c3d8db2dd0ed0cd72
Levil9490 <levil9490@gmail.com>
2026-05-13 16:40:39 -0300

    add: classe Gerenciador

---
+++ ControleDeLivros/src/br/com/levilima/controledelivros/Gerenciador.java
@@ -0,0 +1,8 @@
+package br.com.levilima.controledelivros;
+
+public class Gerenciador {
+	private Livro livros[];
+	static int CODIGO = 0;
+	private int tamanho, id;
+	private String listaDeDesejos = "";
+}
