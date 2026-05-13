5442f46d35818aaf0afe306abb377012bc7289a1
Levil9490 <levil9490@gmail.com>
2026-05-13 17:08:03 -0300

    add: funcionalidade de sair da aplicação

--- ControleDeLivros/src/br/com/levilima/controledelivros/ControleDeLivros.java
+++ ControleDeLivros/src/br/com/levilima/controledelivros/ControleDeLivros.java
@@ -4,8 +4,18 @@ import javax.swing.JOptionPane;
 
 public class ControleDeLivros {
 	public static void main(String[] args) {
-		int opcao;
+		boolean controle = true;
 		
-		opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção:\n1 - Adicionar um livro\n2 - Listar livros\n3 - Buscar livro\n4 - Excluir um livro\n5 - Editar um livro\n6 - Adicionar a lista de desejos\n7 - Ver lista de desejos\n0 - Sair", "Sistema de gerenciamento de livros", 3));
+		
+		
+		while(controle) {
+			int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção:\n1 - Adicionar um livro\n2 - Listar livros\n3 - Buscar livro\n4 - Excluir um livro\n5 - Editar um livro\n6 - Adicionar a lista de desejos\n7 - Ver lista de desejos\n0 - Sair", "Sistema de gerenciamento de livros", 3));
+
+			switch(opcao) {
+				case 0:
+					controle = false;
+					JOptionPane.showMessageDialog(null, "Saindo da aplicação...");
+			}
+		}
 	}
 }
