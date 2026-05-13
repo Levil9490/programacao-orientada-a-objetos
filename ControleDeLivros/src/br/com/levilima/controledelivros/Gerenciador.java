dc7df9261a37dc112752899cda8a26d84a7b9fb5
Levil9490 <levil9490@gmail.com>
2026-05-13 16:44:15 -0300

    add: construtor na classe Gerenciador

--- ControleDeLivros/src/br/com/levilima/controledelivros/Gerenciador.java
+++ ControleDeLivros/src/br/com/levilima/controledelivros/Gerenciador.java
@@ -4,5 +4,12 @@ public class Gerenciador {
 	private Livro livros[];
 	static int CODIGO = 0;
 	private int tamanho, id;
-	private String listaDeDesejos = "";
+	private String listaDeDesejos;
+	
+	public Gerenciador(int tamanho) {
+		this.tamanho = tamanho;
+		this.livros = new Livro[this.tamanho];
+		this.id = 0;
+		this.listaDeDesejos = "";
+	}
 }
