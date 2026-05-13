f919bae367641bcee2458a77e54854f163b27fd0
Levil9490 <levil9490@gmail.com>
2026-05-13 17:23:09 -0300

    add: funcionalidade de adicionar livros

--- ControleDeLivros/src/br/com/levilima/controledelivros/ControleDeLivros.java
+++ ControleDeLivros/src/br/com/levilima/controledelivros/ControleDeLivros.java
@@ -5,13 +5,24 @@ import javax.swing.JOptionPane;
 public class ControleDeLivros {
 	public static void main(String[] args) {
 		boolean controle = true;
+		String nome, descricao;
+		int quantidade;
 		
-		
+		Gerenciador gerenciador = new Gerenciador(5);
 		
 		while(controle) {
 			int opcao = Integer.parseInt(JOptionPane.showInputDialog(null, "Escolha uma opção:\n1 - Adicionar um livro\n2 - Listar livros\n3 - Buscar livro\n4 - Excluir um livro\n5 - Editar um livro\n6 - Adicionar a lista de desejos\n7 - Ver lista de desejos\n0 - Sair", "Sistema de gerenciamento de livros", 3));
 
 			switch(opcao) {
+				case 1:
+					nome = JOptionPane.showInputDialog(null, "Insira um nome para o livro!", "Nome", 1);
+					descricao = JOptionPane.showInputDialog(null, "Insira uma descrição para o livro!", "Descrição", 1);
+					quantidade = Integer.parseInt(JOptionPane.showInputDialog(null, "Insira uma quantidade!", "Quantidade", 1));
+					
+					Livro livro = new Livro(nome, descricao, quantidade);
+					gerenciador.adicionarLivro(livro);
+					
+					break;
 				case 0:
 					controle = false;
 					JOptionPane.showMessageDialog(null, "Saindo da aplicação...");
